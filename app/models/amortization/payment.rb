534a0aa95b99a8388321dc50fc3418723ff356fd
Dileep Nandanam <dileepsnandanam@gmail.com>
2021-04-22 20:59:41 +0530

    adding method to calculate monthly payment

--- app/models/amortization/payment.rb
+++ app/models/amortization/payment.rb
@@ -10,4 +10,12 @@ class Amortization::Payment
     denominator = (1 + @r)**@n - 1
     @p * numerator/denominator
   end
+
+  def self.monthly_payment(principal, num_of_months, interest_rate)
+    new(
+      principal,
+      num_of_months,
+      interest_rate.to_f/100/12
+    ).calculate
+  end
 end
