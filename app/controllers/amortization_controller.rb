f7d8e5fd058072545116782e087da8a2eb7a1d62
Dileep Nandanam <dileepsnandanam@gmail.com>
2021-04-23 14:32:41 +0530

    form error handling

--- app/controllers/amortization_controller.rb
+++ app/controllers/amortization_controller.rb
@@ -9,15 +9,25 @@ class AmortizationController < ApplicationController
     anual_rate = params[:anual_rate]
     terms = params[:terms]
 
+    messages = []
     if [amount, disbursement_date, anual_rate, terms].any?(&:blank?)
-      render json: {message: 'All fields must be present'}, status: 422 and return
-    else
-      amount = amount.to_f
-      anual_rate = anual_rate.to_f
-      terms = terms.to_i
-      disbursement_date = Date.strptime(disbursement_date, "%m/%d/%Y")
+      messages << 'All fields must be present'
+    end
+
+    amount = amount.to_f
+    anual_rate = anual_rate.to_f
+    terms = terms.to_i
+    disbursement_date = Date.strptime(disbursement_date, "%m/%d/%Y")
+    
+    if [amount, disbursement_date, anual_rate, terms].any?{|val| val < 0}
+      messages << 'All values must be positive'
+    end
+
+    if messages.length == 0
       @payment = Amortization::Payment.monthly_payment(amount, terms, anual_rate)
       @schedule_items = Amortization::ScheduleBuilder.new(terms, amount, anual_rate, disbursement_date).build
+    else
+      render json: {messages: messages}, status: 422 and return
     end
   end
 end
--- app/javascript/packs/calculator.js
+++ app/javascript/packs/calculator.js
@@ -1,9 +1,10 @@
 $(document).on('turbolinks:load', () => {
   $('.calculator-form').on('ajax:success', (e) => {
     $('.result').html(e.detail[2].responseText)
+    $('.message').html('')
   })
 
   $('.calculator-form').on('ajax:error', (e) => {
-    $('.message').html(e.detail[0].message)
+    $('.message').html(e.detail[0].messages.join("<br \>"))
   })
 })
