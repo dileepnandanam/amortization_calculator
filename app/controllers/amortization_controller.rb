5fc1d664df0ce1ef9c6553a20538a5c43187a018
Dileep Nandanam <dileepsnandanam@gmail.com>
2021-04-24 20:39:48 +0530

    taking request date instead of disbursement date

--- app/controllers/amortization_controller.rb
+++ app/controllers/amortization_controller.rb
@@ -5,27 +5,28 @@ class AmortizationController < ApplicationController
 
   def calculate
     amount = params[:amount]
-    disbursement_date = params[:disbursement_date]
+    request_date = params[:request_date]
     anual_rate = params[:anual_rate]
     terms = params[:terms]
 
     messages = []
-    if [amount, disbursement_date, anual_rate, terms].any?(&:blank?)
+    if [amount, request_date, anual_rate, terms].any?(&:blank?)
       messages << 'All fields must be present'
     end
 
     amount = amount.to_f
     anual_rate = anual_rate.to_f
     terms = terms.to_i
-    disbursement_date = Date.strptime(disbursement_date, "%m/%d/%Y")
+    request_date = Date.strptime(request_date, "%m/%d/%Y")
     
-    if [amount, disbursement_date, anual_rate, terms].any?{|val| val < 0}
+    if [amount, anual_rate, terms].any?{|val| val < 0}
       messages << 'All values must be positive'
     end
 
+    @disbursement_date = (request_date + 1.months).beginning_of_month
     if messages.length == 0
       @payment = Amortization::Payment.monthly_payment(amount, terms, anual_rate)
-      @schedule_items = Amortization::ScheduleBuilder.new(terms, amount, anual_rate, disbursement_date).build
+      @schedule_items = Amortization::ScheduleBuilder.new(terms, amount, anual_rate, @disbursement_date).build
       render 'calculate', layout: false
     else
       render json: {messages: messages}, status: 422 and return
