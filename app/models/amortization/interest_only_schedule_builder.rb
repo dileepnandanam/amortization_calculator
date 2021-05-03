4530ec3b956d9357179dad3fac46594e899b9e82
Dileep Nandanam <dileepsnandanam@gmail.com>
2021-05-04 02:28:29 +0530

    moving interest only schedule building to function

--- app/models/amortization/interest_only_schedule_builder.rb
+++ app/models/amortization/interest_only_schedule_builder.rb
@@ -1,5 +1,13 @@
 class Amortization::InterestOnlyScheduleBuilder < Amortization::ScheduleBuilder
   def build(interest_only_terms = 3)
+    schedule_items = interest_only_schedule(interest_only_terms)
+    
+    @disbursement_date = (@disbursement_date + interest_only_terms.months).beginning_of_month
+    @terms = @terms - interest_only_terms
+    schedule_items + super()
+  end
+
+  def interest_only_schedule(interest_only_terms)
     schedule_items = []
     fixed_monthly_payment = Amortization::CompoundInterest
       .monthly_interest_for(
@@ -7,6 +15,7 @@ class Amortization::InterestOnlyScheduleBuilder < Amortization::ScheduleBuilder
         @rate
       )
     current_date = @disbursement_date
+    
     (0..interest_only_terms - 1).each do |term|
       current_date = current_date.next_month.beginning_of_month
       schedule_items << Amortization::ScheduleItem.new(
@@ -14,8 +23,7 @@ class Amortization::InterestOnlyScheduleBuilder < Amortization::ScheduleBuilder
       ).build
       @principal = schedule_items.last.end_balance
     end
-    @disbursement_date = current_date
-    @terms = @terms - interest_only_terms
-    schedule_items + super()
+
+    schedule_items
   end
 end
