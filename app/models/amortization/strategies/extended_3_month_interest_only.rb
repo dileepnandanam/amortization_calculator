15328da25dd9a9e2ac10d7fa5dce82f60810591b
Dileep Nandanam <dileepsnandanam@gmail.com>
2021-05-04 17:24:44 +0530

    adding new schedule strategy

---
+++ app/models/amortization/strategies/extended_3_month_interest_only.rb
@@ -0,0 +1,12 @@
+class Amortization::Strategies::Extended3MonthInterestOnly < Amortization::Strategies::First3MonthInterestOnly
+  def build
+    schedule_items = interest_only_schedule(INTEREST_ONLY_TERMS)
+    @disbursement_date = (@disbursement_date + INTEREST_ONLY_TERMS.months).beginning_of_month
+    schedule_items + Amortization::Strategies::Base.new(
+      @terms,
+      @principal,
+      @rate,
+      @disbursement_date
+    ).build
+  end
+end
---
+++ test/models/amortization/strategies/extended_3_month_interest_only_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+
+class Amortization::Strategies::Extended3MonthInterestOnlyTest < ActiveSupport::TestCase
+  test 'should give correct number of schedule items' do
+    terms = 12
+    principal = 10000
+    anual_rate = 10.0
+    disbursement_date = Date.today
+
+    schedule_items = Amortization::Strategies::Extended3MonthInterestOnly.new(
+      terms, principal, anual_rate, disbursement_date
+    ).build
+    assert_equal schedule_items.count, terms + 3
+  end
+
+  test 'should start with next date of disbursement date' do
+    terms = 12
+    principal = 10000
+    anual_rate = 10.0
+    disbursement_date = Date.today
+
+    schedule_items = Amortization::Strategies::Extended3MonthInterestOnly.new(
+      terms, principal, anual_rate, disbursement_date
+    ).build
+    assert_equal schedule_items.first.date, disbursement_date.next_month.beginning_of_month
+  end
+
+  test 'start balence and previous end balence should be same' do
+    terms = 12
+    principal = 10000
+    anual_rate = 10.0
+    disbursement_date = Date.today
+
+    schedule_items = Amortization::Strategies::Extended3MonthInterestOnly.new(
+      terms, principal, anual_rate, disbursement_date
+    ).build
+    (1..schedule_items.count - 1).each do |i|
+      assert_equal schedule_items[i].start_balance, schedule_items[i - 1].end_balance
+    end
+  end
+
+  test '4th month starting balence should be loan principal' do
+    terms = 12
+    principal = 10000
+    anual_rate = 10.0
+    disbursement_date = Date.today
+
+    schedule_items = Amortization::Strategies::Extended3MonthInterestOnly.new(
+      terms, principal, anual_rate, disbursement_date
+    ).build
+    assert_equal schedule_items[3].start_balance, principal
+  end
+end
